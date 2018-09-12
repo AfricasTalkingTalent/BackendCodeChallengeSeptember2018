441091e720e2067283178eb6fa9aa7dbbcee3830
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-09-12 15:33:03 +0300

    housekeeping: invoke getPrimeNumber() in prime_numbers.js

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,3 +1,19 @@
+function getPrimeNumbers(inputValue){
+    //Take user input and return a list of all prime numbers between 0 and the user input
+    console.log('List of all prime numbers between 0 and ' + inputValue);
+    for (let counter = 0; counter <= inputValue; counter++) {
+
+        let notPrime = false;
+        for (let i = 2; i <= counter; i++) {
+            if (counter%i===0 && i!==counter) {
+                notPrime = true;
+            }
+        }
+        if (notPrime === false) {
+            console.log(counter);
+        }
+    }
+}
 // Get process.stdin as the standard input object.
 let standard_input = process.stdin;
 
@@ -18,20 +34,6 @@ standard_input.on('data', function (data) {
     }else
     {
         let number = parseInt(data);
-
-        //Take user input and return a list of all prime numbers between 0 and the user input
-        console.log('List of all prime numbers between 0 and ' + data);
-        for (let counter = 0; counter <= number; counter++) {
-
-            let notPrime = false;
-            for (let i = 2; i <= counter; i++) {
-                if (counter%i===0 && i!==counter) {
-                    notPrime = true;
-                }
-            }
-            if (notPrime === false) {
-                console.log(counter);
-            }
-        }
+        getPrimeNumbers(number);
     }
 });
