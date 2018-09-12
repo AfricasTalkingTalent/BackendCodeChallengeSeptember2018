2cb0c2dcf07aa268b89990eb562e23f183df139d
khwilo <khwilowatai@gmail.com>
2018-09-12 13:45:20 +0300

    Add solution to generating factorial numbers.

---
+++ GenerateFactorialNumbers.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateFactorialNumbers {
+	public static List<Integer> factorial(int n) {
+        List<Integer> factorialList = new ArrayList<Integer>();
+
+        int fact = 1;
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                fact = 1;
+                factorialList.add(fact);
+            } else {
+                fact = fact * i;
+                factorialList.add(fact);
+            }
+        }
+
+        return factorialList;
+    }
+}
